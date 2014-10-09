66fa8fb28e7bcb2dc90a474a6517effc9539c403
Luis Quiros <lquiros@incompanysolutions.com>
2014-10-09 11:49:05 -0600

    make it better

---
+++ app/controllers/Algorit.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import models.Person;
+import play.mvc.Controller;
+
+import java.util.List;
+
+public class Algorit extends Controller {
+
+        public static void aux() {
+	System.out.println("prueba exitosa!");
+    }
+
+
+
+}
